f18ce232f83a2a8832b0aa01e9d99fe82136bff4
jamie-r-davis <jamjam@umich.edu>
2018-09-24 21:47:08 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='commonapp',
+    version='0.0.1',
+    author='Jamie Davis',
+    author_email='jamjam@umich.edu',
+    description='Interface for CommonApp Control Center',
+    install_requires=[
+        'beautifulsoup4',
+        'requests'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jamie-r-davis/commonapp',
+    packages=setuptools.find_packages()
+)
